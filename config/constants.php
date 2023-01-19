8ec60dd02541151f6854e0f3d67f858f0e0d901e
vani-gold <esibevan@yahoo.com>
2023-01-19 22:15:07 +0100

    complete reto app

--- config/constants.php
+++ config/constants.php
@@ -6,11 +6,17 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
     //3 Excecute Query and save Data into Database
+    // define('SITEURL', 'http://localhost/resto_app/');
+    // define('LOCALHOST', 'localhost');
+    // define('DB_USERNAME', 'root');
+    // define('DB_PASSWORD', '');
+    // define('DB_NAME', 'food_order');
+
     define('SITEURL', 'http://localhost/resto_app/');
     define('LOCALHOST', 'localhost');
-    define('DB_USERNAME', 'root');
-    define('DB_PASSWORD', '');
-    define('DB_NAME', 'food_order');
+    define('DB_USERNAME', 'put_your_username_here'); //localhost
+    define('DB_PASSWORD', 'put_your_pswd_here');  //root
+    define('DB_NAME', 'put_your_db_here');
 
     $conn = mysqli_connect(LOCALHOST, DB_USERNAME, DB_PASSWORD) or die(mysqli_error());
     $db_select = mysqli_select_db($conn, DB_NAME) or die(mysqli_error());
